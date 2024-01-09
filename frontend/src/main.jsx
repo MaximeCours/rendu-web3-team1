113f653bfb34ff5661f9c8e0792bf00db29f6335
BAIZEAU Pierre-Antoine <pabaizeau@roole.fr>
2024-01-09 16:33:50 +0100

    add redirection after login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import Login from './login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Login />
+    <App />
 )
