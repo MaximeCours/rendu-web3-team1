9cb05f37c7920f411d5fa318360c38d23c367d65
Theodore Cadet <cadetthe@gmail.com>
2024-01-10 12:28:29 +0100

    fix unicode error, deploy working, answerQuiz and updateQuiz are commented

--- contracts/JavaScriptQuiz.sol
+++ contracts/JavaScriptQuiz.sol
@@ -6,7 +6,7 @@ import "./QuizLibrary.sol";
 contract JavaScriptQuiz {
     using QuizLibrary for QuizLibrary.Quiz[];
 
-    Quiz[] public quizzes;
+    QuizLibrary.Quiz[] public quizzes;
 
     bytes32[] private answerHash;
     address private owner;
@@ -14,118 +14,118 @@ contract JavaScriptQuiz {
     event CorrectAnswer(address responder);
     event WrongAnswer(address responder);
 
-    constructor(bytes32[] _answerHash) {
+    constructor(bytes32[] memory _answerHash) {
         owner = msg.sender;
         answerHash = _answerHash;
         quizzes.push(QuizLibrary.Quiz({
-            question: "Quel est le principal objectif du Web 3.0 ?",
+            question: unicode"Quel est le principal objectif du Web 3.0 ?",
             choices: [
-        "Améliorer la vitesse de connexion Internet.",
-        "Décentraliser les applications et les données.",
-        "Augmenter la résolution des écrans d'ordinateur.",
-        "Réduire la taille des fichiers multimédias."
+        unicode"Améliorer la vitesse de connexion Internet.",
+        unicode"Décentraliser les applications et les données.",
+        unicode"Augmenter la résolution des écrans d'ordinateur.",
+        unicode"Réduire la taille des fichiers multimédias."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Qu'est-ce qu'un smart contract ?",
+            question: unicode"Qu'est-ce qu'un smart contract ?",
             choices: [
-        "Un contrat intelligent entre deux entreprises.",
-        "Un document juridique traditionnel.",
-        "Un accord verbal entre parties contractantes.",
-        "Un programme informatique auto-exécutable sur une blockchain."
+        unicode"Un contrat intelligent entre deux entreprises.",
+        unicode"Un document juridique traditionnel.",
+        unicode"Un accord verbal entre parties contractantes.",
+        unicode"Un programme informatique auto-exécutable sur une blockchain."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Quelle blockchain est principalement associée à l'utilisation de smart contracts ?",
+            question: unicode"Quelle blockchain est principalement associée à l'utilisation de smart contracts ?",
             choices: [
-        "Bitcoin.",
-        "Ethereum.",
-        "Ripple.",
-        "Litecoin."
+        unicode"Bitcoin.",
+        unicode"Ethereum.",
+        unicode"Ripple.",
+        unicode"Litecoin."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Comment fonctionne la décentralisation dans le contexte du Web 3.0 ?",
+            question: unicode"Comment fonctionne la décentralisation dans le contexte du Web 3.0 ?",
             choices: [
-        "Les données sont réparties sur plusieurs nœuds sans contrôle central.",
-        "Toutes les données sont stockées sur un seul serveur.",
-        "Un seul utilisateur a le contrôle total du réseau.",
-        "Les données sont stockées uniquement sur des serveurs gouvernementaux."
+        unicode"Les données sont réparties sur plusieurs nœuds sans contrôle central.",
+        unicode"Toutes les données sont stockées sur un seul serveur.",
+        unicode"Un seul utilisateur a le contrôle total du réseau.",
+        unicode"Les données sont stockées uniquement sur des serveurs gouvernementaux."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Quelle est la caractéristique principale d'un smart contract par rapport à un contrat traditionnel ?",
+            question: unicode"Quelle est la caractéristique principale d'un smart contract par rapport à un contrat traditionnel ?",
             choices: [
-        "Il nécessite une signature manuscrite.",
-        "Il est exécuté automatiquement sans intermédiaire.",
-        "Il peut être annulé à tout moment.",
-        "Il est stocké sur une base de données centralisée."
+        unicode"Il nécessite une signature manuscrite.",
+        unicode"Il est exécuté automatiquement sans intermédiaire.",
+        unicode"Il peut être annulé à tout moment.",
+        unicode"Il est stocké sur une base de données centralisée."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Quel langage de programmation est souvent utilisé pour écrire des smart contracts ?",
+            question: unicode"Quel langage de programmation est souvent utilisé pour écrire des smart contracts ?",
             choices: [
-        "Python.",
-        "JavaScript.",
-        "Solidity.",
-        "Ruby."
+        unicode"Python.",
+        unicode"JavaScript.",
+        unicode"Solidity.",
+        unicode"Ruby."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Qu'est-ce que Hardhat dans le contexte du développement de smart contracts ?",
+            question: unicode"Qu'est-ce que Hardhat dans le contexte du développement de smart contracts ?",
             choices: [
-        "Un environnement de développement pour Ethereum.",
-        "Une casquette résistante.",
-        "Un protocole de sécurité sur Internet.",
-        "Un langage de programmation."
+        unicode"Un environnement de développement pour Ethereum.",
+        unicode"Une casquette résistante.",
+        unicode"Un protocole de sécurité sur Internet.",
+        unicode"Un langage de programmation."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Quel est le rôle principal de MetaMask dans l'écosystème Ethereum ?",
+            question: unicode"Quel est le rôle principal de MetaMask dans l'écosystème Ethereum ?",
             choices: [
-        "Gérer les mots de passe des utilisateurs.",
-        "Faciliter l'échange de cryptomonnaies.",
-        "Fournir une interface utilisateur pour les smart contracts.",
-        "Stocker des fichiers sur la blockchain."
+        unicode"Gérer les mots de passe des utilisateurs.",
+        unicode"Faciliter l'échange de cryptomonnaies.",
+        unicode"Fournir une interface utilisateur pour les smart contracts.",
+        unicode"Stocker des fichiers sur la blockchain."
         ]
         }));
 
         quizzes.push(QuizLibrary.Quiz({
-            question: "Qu'est-ce que Sepolia dans le contexte du Web 3.0 ?",
+            question: unicode"Qu'est-ce que Sepolia dans le contexte du Web 3.0 ?",
             choices: [
-        "Un réseau social décentralisé.",
-        "Un langage de programmation blockchain.",
-        "Un portefeuille électronique.",
-        "Un nouveau protocole de communication."
+        unicode"Un réseau social décentralisé.",
+        unicode"Un langage de programmation blockchain.",
+        unicode"Un portefeuille électronique.",
+        unicode"Un nouveau protocole de communication."
         ]
         }));
 
     }
 
-    function answerQuiz(string memory _response) public {
-        require(msg.sender != owner, "Owner cannot answer the quiz.");
-        if (keccak256(abi.encodePacked(_response)) == answerHash) {
-            emit CorrectAnswer(msg.sender);
-        } else {
-            emit WrongAnswer(msg.sender);
-        }
-    }
+//    function answerQuiz(string memory _response) public {
+//        require(msg.sender != owner, "Owner cannot answer the quiz.");
+//        if (keccak256(abi.encodePacked(_response)) == answerHash) {
+//            emit CorrectAnswer(msg.sender);
+//        } else {
+//            emit WrongAnswer(msg.sender);
+//        }
+//    }
 
-    function updateQuiz(
-        string memory _newQuestion,
-        string[4] memory _newChoices,
-        bytes32 _newAnswerHash
-    ) public {
-        require(msg.sender == owner, "Only owner can update the quiz.");
-        question = _newQuestion;
-        choices = _newChoices;
-        answerHash = _newAnswerHash;
-    }
+//    function updateQuiz(
+//        string memory _newQuestion,
+//        string[4] memory _newChoices,
+//        bytes32 _newAnswerHash
+//    ) public {
+//        require(msg.sender == owner, "Only owner can update the quiz.");
+//        question = _newQuestion;
+//        choices = _newChoices;
+//        answerHash = _newAnswerHash;
+//    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,16 +5,16 @@ async function main() {
     console.log("Deploying contracts with the account:", deployer.address);
     const quizContrat = await ethers.getContractFactory("JavaScriptQuiz");
     const quiz = await quizContrat.deploy([
-            "2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de",
-            "c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
-            "2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de",
-            "c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
-            "044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d",
-            "c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
-            "ad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5",
-            "044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d",
-            "ad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5",
-            "ad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5",
+            "0x2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de",
+            "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
+            "0x2a80e1ef1d7842f27f2e6be0972bb708b9a135c38860dbe73c27c3486c34f4de",
+            "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
+            "0x044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d",
+            "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
+            "0xad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5",
+            "0x044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d",
+            "0xad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5",
+            "0xad7c5bef027816a800da1736444fb58a807ef4c9603b7848673f7e3a68eb14a5",
         ]
     );
 
