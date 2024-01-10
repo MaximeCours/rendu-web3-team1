3e9d9e5d85c65f955241e88644adf1c89b17a62c
Theodore Cadet <cadetthe@gmail.com>
2024-01-10 13:48:15 +0100

    add sepolia network to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.19",
   networks:{
+    sepolia:{
+      url:process.env.API_URL,
+      accounts:[`0x${process.env.PRIVATE_KEY}`]
+    },
     localRes:{
       url:process.env.API_URL,
       accounts:[process.env.PRIVATE_KEY]
